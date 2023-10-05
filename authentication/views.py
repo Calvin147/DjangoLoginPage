39260b6ac772fc3fe57ddbc9b243899344fce204
Calvin <calvindunn147@gmail.com>
2023-10-05 15:55:41 +0200

    Code added to index.html

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def home(request):
-    return HttpResponse("Under construction")
+    return render(request, 'authentication/index.html')
 
 def signup(request):
     return render(request, 'authentication/signup.html')
