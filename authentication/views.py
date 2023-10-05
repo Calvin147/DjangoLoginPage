4b05a1ec26a2ea6abdbaf2438b31ac26b64446b6
Calvin <calvindunn147@gmail.com>
2023-10-05 15:02:41 +0200

    Additional views added

--- authentication/views.py
+++ authentication/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Under construction")
+
+def signup(request):
+    return render(request, 'authentication/signup.html')
+
+def signin(request):
+    return render(request, 'authentication/signin.html')
+
+def signout(request):
+    pass
