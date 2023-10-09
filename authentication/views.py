2558ebd18abfe29c93233b9cbb5fc2c94a44b065
Calvin <calvindunn147@gmail.com>
2023-10-09 15:17:28 +0200

    Adding of Signin view in view.py

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User 
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, 'authentication/index.html')
@@ -29,6 +30,23 @@ def signup(request):
     return render(request, 'authentication/signup.html')
 
 def signin(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+            
+        else:
+            messages.error(request, "Incorrect Credentials")
+            return redirect('home')
+    
+    
     return render(request, 'authentication/signin.html')
 
 def signout(request):
