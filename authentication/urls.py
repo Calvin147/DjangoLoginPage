28f6a139a0a3cf3f5ad2ccc7a1a9d4a2f7f3a3d8
Calvin <calvindunn147@gmail.com>
2023-12-01 11:53:22 +0200

    Addition of task views and models

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import update_task
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
+    path('update_task/<int:task_id>/', update_task, name='update_task'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,51 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from task_manager.models import Task
+from authentication.forms import TaskForm
+
+@login_required
+def view_task(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'authentication/view_tasks.html', {'tasks': tasks})
+
+@login_required
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('view_tasks')
+    else:
+        form = TaskForm()
+        
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'authentication/add_task.html', {'tasks': tasks, 'form': form})
+
+@login_required
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('view_tasks')
+    
+    else: 
+        form = TaskForm(instance=task)
+        
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'authentication/update_task.html', {'tasks': tasks, 'form': form})
+
+@login_required
+def delete_task(request, task_id):
+
 
 def home(request):
     return render(request, 'authentication/index.html')
